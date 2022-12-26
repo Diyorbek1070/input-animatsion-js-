7aa4b0e78fd73b180d8f7b5e5ef18c740adefd6f
Diyorbek1070 <dcharshamboyev@gmail.com>
2022-12-26 15:55:28 +0500

    I made a change to the js

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,16 @@
 const inputEl = document.querySelector(".input");
+const spanEl = document.querySelector(".span");
+const listEl = document.querySelector(".list");
+
+inputEl.addEventListener("mouseover",()=>{
+    if(spanEl.style.top == "18px"){
+        spanEl.style.top = "-15px"
+        spanEl.classList.add("span-border");
+     }
+     else {
+        spanEl.style.top = "18px"
+        spanEl.classList.remove("span-border");
+    } 
+})
+
+
